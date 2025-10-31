d450bd347a3987c9444a71424ebf4372d5ce9e7f
kdudek22 <appleofeden8686@gmail.com>
2025-11-01 00:04:36 +0100

    more updates and tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from scraping.ListingsScraper import ListingsScraper
+from scraping.SingleListingScraper import SingleListingScraper, ListingInfo
 import json
+import attrs
 
 
 import logging
@@ -11,13 +13,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def scrape_listings(url: str) -> set[str]:
-    scraper = ListingsScraper()
-
-    return scraper.scrape(url)
-
-
-if __name__ == "__main__":
+def scrape_listings() -> None:
     links = [
         "https://www.olx.pl/motoryzacja/samochody/q-bmw-e39/",
         "https://www.olx.pl/motoryzacja/samochody/q-bmw-e36/",
@@ -27,14 +23,48 @@ if __name__ == "__main__":
         "https://www.olx.pl/motoryzacja/samochody/q-bmw-drift/",
     ]
     res = set()
+    scraper = ListingsScraper()
 
     for link in links:
-        res = res.union(scrape_listings(link))
+        res = res.union(scraper.scrape(link))
 
     res_list = list(res)
 
-    with open("results.json", "w") as json_file:
+    with open("data/results.json", "w") as json_file:
         json.dump(list(res_list), json_file, indent=4)
     logger.info(f"saved to a file {len(res_list)} links")
 
+
+def scrape_single_listings() -> list[ListingInfo]:
+    urls = json.loads(open("./data/listing_links.json").read())
+
+    urls = [u for u in urls if "otomoto" not in u]  # simply filter otomoto links for now
+
+    res = []
+    scraper = SingleListingScraper()
+
+    for (i, url) in enumerate(urls):
+        try:
+            listing_info = scraper.scrape(url)
+            if listing_info:
+                res.append(listing_info)
+        except Exception as e:
+            logger.exception(f"Error: {e}")
+
+        logger.info(f"Done {i}/{len(urls)}")
+
+    res_dict = [attrs.asdict(listing) for listing in res]
+
+    with open("results.json", "w", encoding="utf-8") as json_file:
+        json.dump(list(res_dict), json_file, indent=4, ensure_ascii=False)
+
+    logger.info(f"saved to a file {len(res_dict)} scraped listings")
+
+
+if __name__ == "__main__":
+    import time
+    start = time.time()
+    scrape_single_listings()
+
+    logger.info(f"Finished in {time.time() - start} seconds")
 
--- scraping/SingleListingScraper.py
+++ scraping/SingleListingScraper.py
@@ -1,11 +1,14 @@
+import random
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 import attrs
 
 import logging
-import SeleniumSelectors
+from scraping import SeleniumSelectors
 
 logger = logging.getLogger(__name__)
 
@@ -27,13 +30,14 @@ class SingleListingScraper:
 
     driver = webdriver.Chrome()
     wait = WebDriverWait(driver, 3)
+    accepted_cookies: bool = False  # as you should only have to accept them once, and it saves us a lot of time
 
-    save_file = "../data/scrapped_links.json"
-
-    def scrape(self, url: str) -> ListingInfo:
+    def scrape(self, url: str) -> ListingInfo | None:
+        time.sleep(random.randint(0, 10))  # they time out if we do it too fast :)
         self.driver.get(url)
 
-        self.accept_cookies()
+        if not self.accepted_cookies:
+            self.accept_cookies()
 
         return self.get_listing_info(url)
 
@@ -45,22 +49,25 @@ class SingleListingScraper:
             )
             element.click()
             logger.info("Accepted cookies")
+            self.accepted_cookies = True
         except:
             logger.warning("Accept cookies banner did not show up")
 
     def get_listing_info(self, url: str) -> ListingInfo:
-        # This contains uncleaned data, but I think it will be easier to clean up after, than while scraping
-        self.wait.until(
-            EC.visibility_of_element_located(SeleniumSelectors.LISTING_PARAMETERS)
-        )
-        return ListingInfo(
-            price=self.driver.find_element(*SeleniumSelectors.LISTING_PRICE).text,
-            url=url,
-            title=self.driver.find_element(*SeleniumSelectors.LISTING_TITLE).text,
-            id=self.driver.find_element(*SeleniumSelectors.LISTING_ID).text,
-            description=self.driver.find_element(*SeleniumSelectors.LISTING_DESCRIPTION).text,
-            parameters=[p.text for p in self.driver.find_element(*SeleniumSelectors.LISTING_PARAMETERS).find_elements(*selenium_selectors.LISTING_PARAMETER)]
-        )
-
+        try:
+            # This contains uncleaned data, but I think it will be easier to clean up after, than while scraping
+            self.wait.until(
+                EC.visibility_of_element_located(SeleniumSelectors.LISTING_PARAMETERS)
+            )
+            return ListingInfo(
+                price=self.driver.find_element(*SeleniumSelectors.LISTING_PRICE).text,
+                url=url,
+                title=self.driver.find_element(*SeleniumSelectors.LISTING_TITLE).text,
+                id=self.driver.find_element(*SeleniumSelectors.LISTING_ID).text,
+                description=self.driver.find_element(*SeleniumSelectors.LISTING_DESCRIPTION).text,
+                parameters=[p.text for p in self.driver.find_element(*SeleniumSelectors.LISTING_PARAMETERS).find_elements(*SeleniumSelectors.LISTING_PARAMETER)]
+            )
+        except:
+            logger.error(f"Could not get listing info for {url}")
 
 
